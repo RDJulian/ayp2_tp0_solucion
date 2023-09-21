6ac88ef990f4489add065c001c3bf80372b4da5a
RDJulian <jruizd@fi.uba.ar>
2023-09-21 11:11:45 -0300

    Agrego comentarios.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 /* NOTA AL LECTOR:
  * ¿Por qué no hay ninguna precondición declarada?
- *
  * Recuerden que las precondiciones, como el nombre indica,
  * declaran condiciones que se deben cumplir para que la
  * función / método funcione de la forma esperada, y genere
@@ -11,12 +10,23 @@
  *
  * Como me aseguré de que no haya NINGUNA condición sobre los
  * parámetros, no necesito declarar ninguna precondición.
+ *
+ * ¿Por qué no hay un Menu?
+ * Decidí no abstraer un Menu porque la lógica es muy acotada
+ * y el main me queda, básicamente, como el control del flujo
+ * del programa. El que abstrajo la clase cumpliendo el
+ * paradigma, está bien.
+ *
+ * ¿Por qué las preguntas o las palabras clave no son constantes?
+ * En mi opinión, tener una constante MIEDO = "miedo" no aporta
+ * nada a nivel legibilidad. Lo mismo si estoy haciendo un cout
+ * de un string. Se sobreentiende que está imprimiendo por
+ * pantalla ese mensaje. El que usó constantes, más que bien.
  */
 
 int main() {
     Fantasma fantasma;
     fantasma.saludar();
-
     std::string pregunta;
     while (pregunta != "salir") {
         std::cout << "Pregunta captada por el Fantasma: ";
@@ -25,7 +35,6 @@ int main() {
             fantasma.recibirPregunta(pregunta);
         }
     }
-
     fantasma.despedir();
     return 0;
 }
